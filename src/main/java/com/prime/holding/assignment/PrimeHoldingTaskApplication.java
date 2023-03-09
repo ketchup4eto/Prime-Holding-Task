c84d7b810f31e63f6e47ef95f9adfb9169689c19
Radoslav <ketchup4eto@abv.bg>
2023-03-09 11:49:56 +0200

    Project setup

---
+++ src/main/java/com/prime/holding/assignment/PrimeHoldingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.prime.holding.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeHoldingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeHoldingTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prime/holding/assignment/PrimeHoldingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime.holding.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeHoldingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
